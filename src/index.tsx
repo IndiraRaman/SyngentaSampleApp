ee9af42ebdca0d9546a557bd7edfc189d84025ad
IndiraRaman <irshs005@gmail.com>
2021-05-21 03:27:07 +0530

    TypeScript Installed

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import {NavigationContainer} from "@react-navigation/native"
+import React from 'react'
+import App from "./App"
+import {Provider} from "react-redux"
+import {store} from "./store"
+
+export default function Root(){
+    return(
+        <Provider store={store}>
+        <NavigationContainer>
+            <App />
+        </NavigationContainer>
+        </Provider>
+    )
+}
